6aca31bc350c02b0dc66841024e145c2d8227b30
Ibrohim571 <ibrohim-01@list.ru>
2023-04-25 17:29:55 +0400

    Assliddin Diplom ishi6

--- router/reouterHome.js
+++ router/reouterHome.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-// const Notebook = require("../modules/notebook");
-router.get("/", (req, res) => {
+const Notebook = require("../modules/notebook");
+router.get("/", async (req, res) => {
   // res.render("index", { isHome: true, title: "Home" });
-  res.send("Hello world");
+  const js = await Notebook.find();
+  res.send(js);
 });
 
 module.exports = router;
