9c60d83a6b146fe0cf7fbdc3ef8ff05514d63ea5
Ibrohim571 <ibrohim-01@list.ru>
2023-04-27 10:31:59 +0400

    Assliddin backend

--- router/reouterHome.js
+++ router/reouterHome.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const router = Router();
 // const Notebook = require("../modules/notebook");
 router.get("/", async (req, res) => {
-  res.render("index", { isHome: true, title: "Home" });
+  // res.render("index", { isHome: true, title: "Home" });
   // const js = await Notebook.find();
   // res.send(js);
+
+  res.sendFile("../pages/contact.html");
 });
 
 module.exports = router;
